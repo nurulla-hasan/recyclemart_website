b9b2edd4a4bb0c8a1ba8f634c351fd6808202234
golap-hasan <golaphasan379@gmail.com>
2026-02-07 17:41:31 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const withNextIntl = createNextIntlPlugin(
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    turbopackFileSystemCacheForDev: true,
+    turbopackFileSystemCacheForDev: true, 
     serverActions: {
       bodySizeLimit: '5mb',
     },
